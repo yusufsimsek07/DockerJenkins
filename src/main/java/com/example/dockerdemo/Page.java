8f4dfa60064c02595989431b7f68bb2d4793b4f5
YUSUF <99399960+yusufsimsek07@users.noreply.github.com>
2024-05-02 15:15:58 +0300

    f

--- src/main/java/com/example/dockerdemo/Page.java
+++ src/main/java/com/example/dockerdemo/Page.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Page {
     @GetMapping
     public String deneme2() {
-        return "YUSUF ÖDEV YAPAR";
+        return "YUSUF ÖDEV Y";
     }
 }
